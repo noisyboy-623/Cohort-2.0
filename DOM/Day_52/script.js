ea8fbb97e10144879e6a7c8580c4b8fc7e7a8cbf
noisyboy-623 <noisyboy2716@gmail.com>
2025-11-28 15:13:44 +0530

    Day 52

---
+++ DOM/Day_52/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("mousemove", (dets) => {
+    document.body.style.setProperty("--x", dets.clientX + 'px')
+    document.body.style.setProperty("--y", dets.clientY + 'px')
+})
