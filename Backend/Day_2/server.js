10d44b91694aeebc59ae920043c0322d64305fd0
noisyboy-623 <noisyboy2716@gmail.com>
2026-01-23 16:17:13 +0530

    Backend Day_2

---
+++ Backend/Day_2/server.js
@@ -0,0 +1,13 @@
+const express = require ('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
+
+app.get('/about', (req, res) => {
+    res.send("This is About page")
+})
+
+app.listen(3000)
