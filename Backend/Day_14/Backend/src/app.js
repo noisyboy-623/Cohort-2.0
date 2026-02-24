8544e66e970523c8f49d41b1586b6566e4eb7468
noisyboy-623 <noisyboy2716@gmail.com>
2026-02-24 21:09:49 +0530

    Backend Day_14, Insta Clone Day-7

---
+++ Backend/Day_14/Backend/src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+
+const authRouter = require("./routes/auth.routes");
+const postRouter = require("./routes/post.routes");
+const userRouter = require("./routes/user.routes");
+
+app.use("/api/auth", authRouter);
+app.use("/api/post", postRouter);
+app.use("/api/user", userRouter);
+
+module.exports = app;
