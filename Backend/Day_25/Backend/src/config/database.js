1fb71408b31671353959cc26132d46c1c6e99b4d
noisyboy-623 <noisyboy2716@gmail.com>
2026-03-09 21:41:00 +0530

    Backend Day_25, Moodify Day-2

---
+++ Backend/Day_25/Backend/src/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+function connectToDB() {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Connected to database");
+    })
+    .catch((err) => {
+      console.log("Error connecting to database", err);
+    });
+}
+
+module.exports = connectToDB;
