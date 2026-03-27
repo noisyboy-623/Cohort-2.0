888ef621f432de775f5b5289457addcbfcd47977
noisyboy-623 <noisyboy2716@gmail.com>
2026-03-28 00:40:51 +0530

    Backend Day_31

---
+++ Backend/Day_31/server.js
@@ -0,0 +1,20 @@
+import app from "./src/app.js"
+import { createServer } from "http";
+import  { Server } from "socket.io";
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  console.log("new connection created")
+  socket.on("message", (msg)=> {
+    console.log("user fired message event")
+    console.log(msg)
+
+    io.emit("abc")
+  })
+});
+
+httpServer.listen(3000, ()=>{
+    console.log("Server running on port 3000")
+})
